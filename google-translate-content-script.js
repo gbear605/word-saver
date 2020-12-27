f8d7e182b50077b9a368854fbd8c934841819bda
gbear605 <developer@vivissi.com>
2020-12-27 17:38:38 -0500

    Fix bug with google translate; hopefully fix bug with chrome upload

--- google-translate-content-script.js
+++ google-translate-content-script.js
@@ -2,7 +2,7 @@ let saveButton = document.createElement("button");
 saveButton.textContent = "Save to Word Saver";
 saveButton.value = "Save";
 saveButton.onclick = () => {
-  let originalWord = document.querySelector('[aria-label="Source text"]').textContent;
+  let originalWord = document.querySelector('[aria-label="Source text"]').value;
   let originalLanguage = document.querySelector('[aria-label="Source text"]').parentElement.parentElement.parentElement.lang;
   let definition = document.querySelector('[data-language-to-translate-into]').childNodes[0].textContent;
   let definitionLanguage = document.querySelector('[data-language-to-translate-into]').parentElement.lang;
