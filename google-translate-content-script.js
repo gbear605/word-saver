f9629136f4f15f2549f43334331cf798f102adc6
gbear605 <developer@vivissi.com>
2020-12-27 16:22:58 -0500

    Finish adding Google Translate support

--- google-translate-content-script.js
+++ google-translate-content-script.js
@@ -1,14 +1,15 @@
 let saveButton = document.createElement("button");
-saveButton.textContent = "Save";
+saveButton.textContent = "Save to Word Saver";
 saveButton.value = "Save";
 saveButton.onclick = () => {
-  let originalWord = null;
-  let originalLanguage = null;
-  let definition = null;
-  let definitionLanguage = null;
-
-  saveWord(originalWord, definition, convertFullNameToISO(originalLanguage), convertFullNameToISO(definitionLanguage));
+  let originalWord = document.querySelector('[aria-label="Source text"]').textContent;
+  let originalLanguage = document.querySelector('[aria-label="Source text"]').parentElement.parentElement.parentElement.lang;
+  let definition = document.querySelector('[data-language-to-translate-into]').childNodes[0].textContent;
+  let definitionLanguage = document.querySelector('[data-language-to-translate-into]').parentElement.lang;
+  saveWord(originalWord, definition, originalLanguage, definitionLanguage);
 }
 
-document.querySelector("[aria-labelledby=\"i4\"]").appendChild(saveButton);
+let buttonRow = document.querySelectorAll('nav')[1];
+saveButton.className = buttonRow.childNodes[2].childNodes[0].className;
+buttonRow.appendChild(saveButton);
 
