811dd61f6fdd11857ce3fb98bec4e627d09fca7f
gbear605 <developer@vivissi.com>
2021-03-29 20:09:15 -0400

    Fix Glosbe

--- glosbe-content-script.js
+++ glosbe-content-script.js
@@ -1,82 +1,69 @@
-let urlSplits = document.URL.split('/');
+let urlSplits = document.URL.split("/");
 let originalLanguage = urlSplits[urlSplits.length-3];
 let definitionLanguage = urlSplits[urlSplits.length-2];
 let originalWord = urlSplits[urlSplits.length-1].replace("%20", " ");
 
-for (row of document.querySelectorAll("li.phraseMeaning")) {
+for (translationArea of document.querySelectorAll(".phrase__translation")) {
 
-  let metaInformationNode = row.parentNode.previousElementSibling;
-  while (metaInformationNode != null && metaInformationNode.className != "defmetas") {
-    metaInformationNode = metaInformationNode.previousElementSibling;
+  let originalGenderAndPhraseInformation = Array.from(translationArea.querySelectorAll(".phrase__summary__field")).map((x) => x.textContent);
+  let originalGender = "";
+  if (originalGenderAndPhraseInformation.includes("feminine")) {
+    originalGender = "f";
+  } else if (originalGenderAndPhraseInformation.includes("masculine")) {
+    originalGender = "m";
+  } else if (originalGenderAndPhraseInformation.includes("neuter")) {
+    originalGender = "n";
   }
-  let partOfSpeech = null;
-  let originalWordGender = null;
-  if (metaInformationNode != null) {
-    for (metaInformationInnerNode of metaInformationNode.querySelectorAll('.defmeta')) {
-      if (metaInformationInnerNode.textContent == 'Gender:') {
-        originalWordGender = metaInformationInnerNode.nextElementSibling.textContent.trim().replace(';', '');
-        if (originalWordGender == 'feminine') {
-          originalWordGender = 'f'
-        } else if (originalWordGender == 'masculine') {
-          originalWordGender = 'm'
-        } else if (originalWordGender == 'neuter') {
-          originalWordGender = 'n'
-        }
+
+  for (row of translationArea.querySelectorAll(".translation__item")) {
+    let definition = row.querySelector(".translation__item__phrase").textContent
+    let definitionGender = "";
+    let definitionGenderAndPhraseInformation = Array.from(row.querySelectorAll(".phrase__summary__field")).map((x) => x.textContent);
+    if (definitionGenderAndPhraseInformation.includes("feminine")) {
+      definitionGender = "f";
+    } else if (definitionGenderAndPhraseInformation.includes("masculine")) {
+      definitionGender = "m";
+    } else if (definitionGenderAndPhraseInformation.includes("neuter")) {
+      definitionGender = "n";
+    }
+
+    let definitionInfo = "";
+    let definitionPartOfSpeech = "";
+    if (definitionGenderAndPhraseInformation.includes("noun")) {
+      definitionPartOfSpeech = "noun";
+    }
+    if (definitionLanguage == "de") {
+      if (definitionPartOfSpeech == "noun") {
+        definitionInfo = "N" + definitionGender;
       }
-      if (metaInformationInnerNode.textContent == 'Type:') {
-        partOfSpeech = metaInformationInnerNode.nextElementSibling.textContent.trim().replace(';', '');
+    }
+    if (definitionLanguage == "es" || definitionLanguage == "fr") {
+      if (definitionPartOfSpeech == "noun") {
+        definitionInfo = "n" + definitionGender;
       }
     }
-  }
 
-  let definition = row.querySelector(".text-info strong").textContent;
-  let definitionGender = null;
-  let genderAndPhraseInformation = row.querySelector(".gender-n-phrase").textContent.replace('{', '').replace('}', '').trim()
-  if (genderAndPhraseInformation == "noun feminine") {
-    definitionGender = 'f'
-  } else if (genderAndPhraseInformation == 'noun masculine') {
-    definitionGender = 'm'
-  } else if (genderAndPhraseInformation == 'noun neuter') {
-    definitionGender = 'n'
-  }
-  
-  let originalWordInfo = null;
-  if(originalLanguage == "de") {
-    if(partOfSpeech == "noun") {
-      originalWordInfo = "N" + originalWordGender;
+    let originalInfo = "";
+    if (originalLanguage == "de") {
+      if (definitionPartOfSpeech == "noun") {
+        originalInfo = "N" + originalGender;
+      }
     }
-  }
-  if(originalLanguage == "es" || originalLanguage == "fr") {
-    if(partOfSpeech == "noun") {
-      originalWordInfo = "n" + originalWordGender;
+    if (originalLanguage == "es" || originalLanguage == "fr") {
+      if (definitionPartOfSpeech == "noun") {
+        originalInfo = "n" + originalGender;
+      }
     }
-  }
-  if(originalWordInfo == null) {
-    originalWordInfo = partOfSpeech; // We haven't handled this case
-  }
 
-  let definitionInfo = null;
-  if(definitionLanguage == "de") {
-    if(partOfSpeech == "noun") {
-      definitionInfo = "N" + definitionGender;
-    }
-  }
-  if(definitionLanguage == "es" || definitionLanguage == "fr") {
-    if(partOfSpeech == "noun") {
-      definitionInfo = "n" + definitionGender;
-    }
-  }
-  if(definitionInfo == null) {
-    definitionInfo = partOfSpeech; // We haven't handled this case
-  }
-
-  let originalWordWithInformation = processWord(originalWord, originalWordInfo, originalLanguage);
-  definition = processWord(definition, definitionInfo, definitionLanguage);
+    let originalWordWithInformation = processWord(originalWord, originalInfo, originalLanguage);
+    definition = processWord(definition, definitionInfo, definitionLanguage);
 
 
-  var saveButton = document.createElement("input");
-  saveButton.type = "button";
-  saveButton.value = "Save";
-  saveButton.onclick = () => saveWord(originalWordWithInformation, definition, originalLanguage, definitionLanguage);
-  row.appendChild(saveButton);
+    var saveButton = document.createElement("input");
+    saveButton.type = "button";
+    saveButton.value = "Save to Word Saver";
+    saveButton.className = "button-sm"
+    saveButton.onclick = () => saveWord(originalWordWithInformation, definition, originalLanguage, definitionLanguage);
+    row.appendChild(saveButton);
+  }
 }
